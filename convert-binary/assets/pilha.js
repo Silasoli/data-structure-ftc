f5f835d801a79d91c45f3f8a9b105cbedecfabcf
Silasoli <silas.oliveira@blockhub.builders>
2022-08-31 21:28:45 -0300

    calc binary

---
+++ convert-binary/assets/pilha.js
@@ -0,0 +1,19 @@
+function decimalTobin(decimal) {
+  const array = new Pilha();
+  let number = document.getElementById("num").value;
+  let rest;
+  let numerBinaryInString = "";
+  while (number > 0) {
+    rest = Math.floor(number % 2);
+    array.pushStack(rest);
+    number = Math.floor(number / 2);
+  }
+  while (!array.isEmptyStack()) {
+    numerBinaryInString += array.popStack();
+  }
+
+  return console.log(`${numerBinaryInString}`);
+}
+
+
+
