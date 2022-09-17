6a3cafb65740524a731d7bc47042aaeb75160265
Silasoli <silas.oliveira@blockhub.builders>
2022-09-17 17:14:55 -0300

    Finalizando atividade de fila

---
+++ handling-queue/assets/script.js
@@ -0,0 +1,65 @@
+const fila = new Queue();
+
+fila.enqueue("Silas");
+fila.enqueue("Sabrina");
+fila.enqueue("Levi");
+fila.enqueue("Kadmo");
+
+addOptions();
+showQtd();
+
+function addOptions() {
+  const itens = fila.toString().split(",");
+  if (fila.size() < 1) return;
+  itens.forEach(function callback(value, index) {
+    const select = document.getElementById("select");
+    const option = new Option(value.trim(), index);
+    select.add(option);
+  });
+}
+
+function deleteOptions() {
+  const select = document.getElementById("select");
+  const selectLength = select.options.length;
+  for (i = selectLength; i >= 1; i--) {
+    select.remove(i);
+  }
+}
+
+function removeItem() {
+  deleteOptions();
+  showLastRemoved(fila.peek());
+  fila.dequeue();
+  showQtd();
+  addOptions();
+  alertResult('removido')
+}
+
+function addItem() {
+  deleteOptions();
+  const input = document.getElementById("num");
+  fila.enqueue(input.value);
+  input.value = "";
+  showQtd();
+  addOptions();
+  alertResult('adicionado')
+}
+
+function showQtd() {
+  const btn = document.getElementById("qtdPersons");
+  btn.textContent = `Quantidade de pessoas: ${fila.size()}`;
+}
+
+function showLastRemoved(name) {
+  const btn = document.getElementById("lastRemoved");
+  if (name === undefined) btn.textContent = `A fila está vazia`;
+  else btn.textContent = `Último item removido: ${name}`;
+}
+
+
+function alertResult(result) {
+  return Swal.fire({
+    icon: 'success',
+    title: `Item ${result} com sucesso`,
+  });
+}
